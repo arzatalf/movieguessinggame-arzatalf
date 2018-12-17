f2d7b8374d7e02e69ea6a95caba7c22d647ceb58
arzatalf <arzatalf@011-4303-S02GQ.students.cisd.conroeisd.loc>
2018-12-17 08:47:57 -0600

    I added a way so you arent punished for guessing the same letter

--- src/moviegame/Game.java
+++ src/moviegame/Game.java
@@ -6,122 +6,125 @@
 package moviegame;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
 
 /**
  *
- * @author rbowlen
+ * @author arzatalf
  */
 public class Game {
-    
-    
-    /**
-     * Reads movie.txt from the project folder and returns each title within 
-     * as an ArrayList.
-     * @return ArrayList<String> movieList
-     */
-    private ArrayList<String> listMovies() {
-        // To store each movie title in movies.txt
-        ArrayList<String> movieList = new ArrayList<>();
-        
+    private ArrayList<String> readMovies() {
+        ArrayList <String> movies = new ArrayList<>();
         try {
             Scanner scan = new Scanner(new File("movies.txt"));
- 
+        
             while(scan.hasNextLine()) {
-                movieList.add(scan.nextLine());
-            } 
-            
-        } catch(FileNotFoundException e) {
-                    System.out.println("Movies text file was not found. Please"
-                            + " add and try again.");
-                    }
-        return movieList;
-    }
-    
-    /**
-     * Selects a movie randomly from the movies text file.
-     * @return String randomMovie
-     */
-    private String getRandMovie() {
-        ArrayList<String> movieList = listMovies();
-        int index = (int)(Math.random() * movieList.size());
-        return movieList.get(index);
-        
-    }
-    
-    /**
-     * Replaces all movie titles with underscores
-     * @param movieTitle
-     * @return 
-     */
-    public String obscureTitle(String movieTitle, String correctLetters) {
-        if(correctLetters == "") {
-            return movieTitle.replaceAll("[a-zA-Z]", "_");
-        } else {
-            return movieTitle.replaceAll("[a-zA-Z&&[^" + correctLetters + "]]", "_");
-        }
-    }
-    
-    public String inputLetter() {
-        System.out.println("Guess a letter:");
-        //Create a scanner to get user's guess.
-        Scanner scan = new Scanner(System.in);
-        //Store user guess for processing
-        String letter = scan.nextLine().toLowerCase();
-        
-        if(!letter.matches("[a-z]")) {
-            System.out.println("This is not a letter. Try again.");
-            return inputLetter();
-        }
-        
-        return letter;
-        
-    }
-    
-    /**
-     * Checks if the user's guess was correct
-     * @param movieTitle
-     * @param guess
-     * @return boolean
-     */
-    public boolean checkGuess(String movieTitle, String guess) {
-        return movieTitle.contains(guess);
-    }
-    
-    public void initGame() {
-        // Store correct guesses
-        String correctLetters = "";
-        // Store incorrect guesses
-        String wrongLetters = "";
-        // User chances
-        int livesLeft = 3;
-        // Get a movie title
-        String movie = getRandMovie();
-        // Game over
-        boolean gameOver = false;
-        
-        while(!gameOver) {
-            // Obscure the title
-            System.out.println(obscureTitle(movie, correctLetters));
-
-
-            // Get a user guess
-            String guess = inputLetter();
-            //Check to see if the users guess is in the title
-            if(checkGuess(movie, guess)) {
-                correctLetters += guess;
-                System.out.println("Correct!");
-            } else {
-                wrongLetters += guess;
-                System.out.println("Sorry, try again!");
-                livesLeft--;
+                movies.add(scan.nextLine());
             }
-        }   
+        } catch (FileNotFoundException e) {
+            System.out.println("A movies file was not found. please put a file"
+                    + " named movies.txt in your project folder");
+        }
         
-       
+        return movies;
+        }
         
+        private String getRandomMovie() {
+            ArrayList<String> movies = readMovies();
+            int index = (int)(Math.random()*((movies.size()-0)+1))+0;
+            return movies.get(index);
+        }
+
+        public String obscureMovie(String movieTitle, String correctLetters) {
+            if (correctLetters == "") {
+            return movieTitle.replaceAll("[a-zA-z]","_");
+        } else {
+                return movieTitle.replaceAll("[a-zA-Z&&[^"+correctLetters +"]]","_");
+            }
+        }
+        public String inputLetter() {
+            System.out.println("guess a letter:");
+            //create a scanner to capture user data
+            Scanner scan = new Scanner(System.in);
+            //store the letter that was guessed
+            String letter = scan.next().toLowerCase();
+
+            
+            if(!letter.matches("[a-z]")) {
+                System.out.println("this is not a letter. Please try again");
+                return inputLetter();
+            }
+            return letter;
+        }
+        /**
+         * checks if the users guess is in the title
+         * @param movieTitle
+         * @param guess
+         * @return boolean
+         */
+        public boolean checkGuess(String movieTitle, String guess) {
+            return movieTitle.contains(guess);   
+        }
+        
+        public void initGame() {
+            boolean gameWon = false;
+                //Get a movie title
+                String movie = getRandomMovie();
+                
+                //stores incorrect letters guessed
+                String wrongLetters= "";
+                
+                //stores correct letters guessed
+                String correctLetters= "";
+                
+                //creates lives 
+                int livesLeft =3;
+                
+                //string that decides when the game is over
+                String GameOver= "GameOver";
+                
+                while(!gameWon &&(livesLeft>0)) {
+                    
+                //get an obscured line
+                String obscure = obscureMovie (movie, correctLetters);
+                System.out.println(obscure);
+                
+                //create the exit for the loop
+                if (obscure.equals(movie)) {
+                    System.out.println("You won!");
+                    break;
+                }
+                
+                // store a user's guess
+                String guess = inputLetter();
+
+
+                //check if the user was correct
+                checkGuess(movie, guess);
+                
+                
+                //checks if you guessed the same letter
+                if ((wrongLetters.contains(guess)||(correctLetters.contains(guess)))){
+                    System.out.println("you already guessed "+guess);
+                   
+                //checks if you got the letter right
+                } else if (checkGuess(movie, guess)) {
+                    correctLetters += guess;
+                    System.out.println("Correct!");
+                    //checks if guess is wrong
+                }else {
+                    System.out.println("Sorry "+guess+" is wrong, try again!");
+                    livesLeft--;
+                    wrongLetters += guess;
+                } 
+                    
+                    //tells you when you are out of lives
+                if (livesLeft<1) {
+                    System.out.println("GameOver the movie was "+movie);
+                }
+                
+        }        
     }
-    
 }
